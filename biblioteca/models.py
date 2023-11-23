39fa9c021bc2469ea81d5a188c8318d297b79d3f
PatriciaRisGar <xprisgar115@ieshnosmachado.org>
2023-11-23 20:33:25 +0100

    [libreria-PatriciaRisGar] - Modelo

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,11 +1,46 @@
-from django.contrib.auth.models import AbstractServer
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
-class User(AbstractServer):
-    dni = models.CharField(max_length='9')
-    adress = models.CharField()
-    telefono = models.PhoneNumberField()
-    
+class User(AbstractUser):
+
+    dni = models.CharField(max_length=9)
+    address = models.CharField(max_length=30)
+    phoneNumber = models.IntegerField()
+
+class Book(models.Model):
+    STATUS = [('available', 'Available'),('loaned', 'Loaned'), ('loanProcess','In loan process'),]
+
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=30)
+    publisher =  models.ForeignKey('Publisher',on_delete=models.CASCADE)
+    publicationDate = models.DateField()
+    genre = models.CharField(max_length=10)
+    isbn = models.CharField(max_length=10, unique = True)
+    sumary = models.TextField()
+    availability = models.CharField(max_length=11,choices=STATUS, default='available')
+    cover = models.ImageField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    biograohy = models.TextField()
+    photo = models.ImageField()
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    website = models.URLField()
+
+
+class Loan (models.Model):
+    STATUS = [('loaned', 'Loaned'), ('returned','Returned')]
+
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    loanDate = models.DateField()
+    returnDate = models.DateField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    status = models.CharField(max_length=11,choices=STATUS, default='loaned')
 
--- mySite/settings.py
+++ mySite/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'biblioteca.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
