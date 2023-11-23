5033434f0a7635e2eb8ce596d30d4e95a1f46466
PatriciaRisGar <xprisgar115@ieshnosmachado.org>
2023-11-23 12:31:56 +0100

    [libreria-PatriciaRisGar] - Inicio

---
+++ biblioteca/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractServer
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractServer):
+    dni = models.CharField(max_length='9')
+    adress = models.CharField()
+    telefono = models.PhoneNumberField()
+
